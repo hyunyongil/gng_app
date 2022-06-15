a222e7ba76aed09e495443b3a25dcfc5a289927a
xyr <cy56@ggook.com>
2022-06-15 19:59:40 +0800

    20220615

--- lib/main.dart
+++ lib/main.dart
@@ -24,13 +24,40 @@ class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Center(
-      child: Text('你好Flutter 222',
-          textDirection: TextDirection.ltr,
-          style: TextStyle(
-            fontSize: 20.5,
-            color: Colors.red,
-            //color: Color.fromRGBO(244, 233, 121, 0.5)
-          )),
-    );
+        child: Container(
+          child: const Text('我是一个文本我是一个文本我是一个文本我是一个文本我是一个文本我是一个文本',
+              textAlign: TextAlign.left,
+              overflow: TextOverflow.ellipsis,
+              maxLines: 2,
+              textScaleFactor: 1.8,
+              style: TextStyle(
+                  fontSize: 16.0,
+                  color: Colors.red,
+                  fontWeight: FontWeight.w800,
+                  fontStyle: FontStyle.italic,
+                  decoration: TextDecoration.lineThrough,
+                  decorationColor: Colors.white,
+                  decorationStyle: TextDecorationStyle.dashed,
+                  letterSpacing: 5.0)),
+          height: 300.0,
+          width: 300.0,
+          decoration: BoxDecoration(
+            color: Colors.yellow,
+            border: Border.all(
+              color: Colors.blue, 
+              width: 2.0
+            ),
+            borderRadius: const BorderRadius.all(
+              Radius.circular(10)
+            )
+          ),
+          //padding: EdgeInsets.fromLTRB(10,30,5,0),
+          padding: EdgeInsets.all(10),
+          margin: EdgeInsets.all(10),
+          //transform: Matrix4.translationValues(100, 0, 0)
+          //transform: Matrix4.rotationZ(-0.3),
+          //transform: Matrix4.diagonal3Values(1.2, 1, 1),
+          alignment: Alignment.topLeft,
+    ));
   }
 }
