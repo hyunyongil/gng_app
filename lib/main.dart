46e487282d64eaddf00213b96930e50d1770d3fc
xyr <cy56@ggook.com>
2022-06-22 18:58:37 +0800

    2022-06-22

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+// ignore_for_file: deprecated_member_use, unnecessary_this
 import 'package:flutter/material.dart';
 import 'package:flutter/rendering.dart';
 import 'res/listData.dart';
@@ -25,32 +26,46 @@ class MyApp extends StatelessWidget {
 class HomeContent extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return ListView(
-      children: listData.map((value) {
-        return Card(
-          margin: EdgeInsets.all(10),
-          child: Column(
-            children: <Widget>[
-              AspectRatio(
-                  aspectRatio: 16 / 9,
-                  child: Image.network(
-                    value["imageUrl"],
-                    fit: BoxFit.cover,
-                  )),
-              ListTile(
-                  leading: CircleAvatar(
-                    backgroundImage: NetworkImage(value["imageUrl"]),
-                  ),
-                  title: Text(value["title"]),
-                  subtitle: Text(
-                    value["description"],
-                    overflow: TextOverflow.ellipsis,
-                    maxLines: 1,
-                  ))
-            ],
-          ),
-        );
-      }).toList(),
+    return Container(
+      padding: EdgeInsets.all(10),
+      height: 600,
+      width: 400,
+      color: Colors.pink,
+      child: Wrap(
+        spacing: 10,
+        runSpacing: 10,
+        direction: Axis.vertical,
+        //runAlignment: WrapAlignment.end,
+        //alignment: WrapAlignment.center,
+        children: <Widget>[
+          MyButton('第1季'),
+          MyButton('第2季第2季'),
+          MyButton('第3季'),
+          MyButton('第4季第4'),
+          MyButton('第5季'),
+          MyButton('第6季第6季第6季第6季'),
+          MyButton('第7季第'),
+          MyButton('第8季'),
+          MyButton('第9季'),
+          MyButton('第10季第10季'),
+        ],
+      ),
+    );
+  }
+}
+
+class MyButton extends StatelessWidget {
+  final String txt;
+  //const MyButton(this.txt);
+  const MyButton(this.txt, {Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // TODO: implement build
+    return RaisedButton(
+      onPressed: () {},
+      child: Text(this.txt),
+      textColor: Theme.of(context).accentColor,
     );
   }
 }
